bbecaee029ed2f73bb809016ebfb6c0a1d2e1699
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2025-01-16 14:44:13 +0800

    android support (unfinished)

--- OpenUtau.Android/MainActivity.cs
+++ OpenUtau.Android/MainActivity.cs
@@ -1,21 +1,27 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Widget;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 using Avalonia;
 using Avalonia.Android;
+using Avalonia.ReactiveUI;
+using OpenUtau.App;
 
-namespace OpenUtau.Android;
-
-[Activity(
-    Label = "OpenUtau.Android",
-    Theme = "@style/MyTheme.NoActionBar",
-    Icon = "@drawable/icon",
-    MainLauncher = true,
-    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
-{
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+namespace OpenUtau.Android {
+    [Activity(
+        Label = "OpenUtau.Android",
+        Theme = "@style/MyTheme.NoActionBar",
+        Icon = "@drawable/icon",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    public class MainActivity : AvaloniaMainActivity<OpenUtau.App.App>
     {
-        return base.CustomizeAppBuilder(builder)
-            .WithInterFont();
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
     }
 }
